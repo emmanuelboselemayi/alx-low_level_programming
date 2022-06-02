60d9c23fa87f9680968250462ecb1be48ee8e62a
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-02 14:49:38 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
-  * main - program entry point
-  * Return: 0
-  */
+ * main - program entry point
+ * Return: 0
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");

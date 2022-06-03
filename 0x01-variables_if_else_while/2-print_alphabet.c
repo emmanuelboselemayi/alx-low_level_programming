afbe8d0b0cfa66f877afe10522f76a0008860506
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-03 15:31:51 -0700

    C/C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Program entry point
- *   * Description: Prints the letters of the alphabet in lowercase
- *    * Return: 0
- *     */
+ * main - Program entry point
+ * Description: Prints the letters of the alphabet in lowercase
+ * Return: 0
+ */
 int main(void)
 {
 		char x = 'a';

bc189ec616d4791298041475d92547021e2cc553
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-03 17:35:32 -0700

    mxxm

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,23 +6,24 @@
  * Return: 0
  */
 int main(void)
-{ 
-	int x = 0;
+{
+		int x = 0;
 
-	while (x < 10)
-	{	
-		putchar(48 + x);
+		while (x < 10)
+		{
+			putchar(48 + x);
 
-		if (x != 9)
-		{	
-			putchar(',');
-			putchar(' ');
+			if (x != 9)
+			{
+			       putchar(',');
+																				               putchar(' ');
+       																						}
+
+				x++;
 		}
-		x++;
-	}
-	
-	putchar('\n');
 
-	return (0);
-} 	
+		putchar('\n');
+
+		return (0);
+}
 

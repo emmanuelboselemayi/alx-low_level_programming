98e53a2ec0ec81c246b172efa560cb26015f1ac2
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-07 18:40:01 -0700

    find letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isalpha - character check
+* Description: checks to see if a character is an actual letter
+* @c:
+* Return: 1 for letter and 0 if otherwise 
+*/
+int _isalpha(int c)
+{
+		int result = (c >= 97 && c <= 122) || (c >= 65 && c <= 90) ? 1 : 0;
+
+		return (result);
+}

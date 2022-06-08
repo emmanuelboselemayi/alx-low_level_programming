fb568a4e3b253761057fde7bd96f6032381eaada
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-07 18:47:32 -0700

    update3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _isalpha - character check
-* Description: checks to see if a character is an actual letter
-* @c:Return 1
-* Return: 1 for letter and 0 if otherwise 
-*/
+ * _isalpha - character check
+ * Description: checks to see if a character is an actual letter
+ * @c:Return 1
+ * Return: 1 for letter and 0 if otherwise 
+ */
 int _isalpha(int c)
 {
 		int result = (c >= 97 && c <= 122) || (c >= 65 && c <= 90) ? 1 : 0;

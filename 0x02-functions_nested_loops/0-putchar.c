231778ac96c0d414ec5d15be3dfe4d408b2fd007
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-08 12:56:10 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- * main - prints _putchar.
- * Return: 0 on success.
+ * main - _putchar entry
+ * Description: Prints _putchar
+ * Return: 0
  */
 int main(void)
 {
-		char c[] = "_putchar";
+		char c[9] = "_putchar";
 		int i;
-
-		for (i = 0; i < 9; i++)
+                for (i = 0; i < 8; i++)
 		{
 			_putchar(c[i]);
 		}
+
 		_putchar('\n');
+
 		return (0);
 }
 

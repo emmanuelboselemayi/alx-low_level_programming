e40d672d6648d22ba3396f6430be0759e546c328
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-07 17:42:09 -0700

    a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet from a-z.
+ * Return: void
+ */
+void print_alphabet(void)
+{
+		char x;
+
+		for (x= 'a'; x <= 'z'; x++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+}

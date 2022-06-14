be3bc2f0c693a70964109f9326dbdc260587ed01
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-13 17:21:57 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void _puts(char *str)
 {
-	    char *str;
-
-	        str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-		_puts(str);
-		 return (0);
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
 }

9488d0c74d04b5f7c1f342292bb5612ec8ea7a98
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-13 17:17:13 -0700

    stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints a string flollowed a new line 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	    char *str;
+
+	        str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+		_puts(str);
+		 return (0);
+}

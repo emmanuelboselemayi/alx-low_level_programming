cb980439a577632984a89df02a3193033360d4e7
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-13 18:12:05 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - check code 
+ * Description: Prints every other character of a string
+ * @str: pointer int type
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+		int x = 0;
+		int i = 0;
+
+		for (x = 0; str[x] != '\0'; x++)
+		{
+		}
+
+		for (i = (x + 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+
+		_putchar('\n');
+}

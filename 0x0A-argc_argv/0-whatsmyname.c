de298ccf9f69abaf588693587fc539ff833ef53b
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-06-24 16:53:17 -0700

    c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints buffer in hexa
- * Description: Prints the name of the program
- * @argc: The number of arguments passed to the program
- * @argv: Array of strings arguments that were passed to the program
- * Return: 0
- */
+* main - check code
+* Description: Prints the name of the program
+* @argc: The number of arguments passed to the program
+* @argv: Array of strings arguments that were passed to the program
+* Return: 0
+*/
 int main(int argc __attribute__((unused)), char const *argv[])
 {
 	printf("%s\n", argv[0]);

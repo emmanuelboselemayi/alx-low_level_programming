0f385bbbc1a5cb05904d0013742c61176cd652e4
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-07-05 10:20:20 -0700

    fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * *main - prints the name of
- * *the file it was compiled from
- * *
- * *Return: nothing to be returned
- * */
+*main - prints the name of
+*the file it was compiled from
+*
+*Return: nothing to be returned
+*/
 
 int main(void)
 {

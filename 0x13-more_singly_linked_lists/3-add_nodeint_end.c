99411a56e2e490c3d70b33633517ce1f487e511d
emmanuelboselemayi <hyperfacenzo@gmail.com>
2022-07-19 15:23:58 -0700

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,36 +1,37 @@
 #include "lists.h"
-
 /**
- * add_nodeint_end - adds a new node at theend of a listint_t list.
- * @head: head node
- * @n: number of input nodes
- * Return: address of the new element
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: double pointer to listint_t
+ * @n: number of elements in listint_t
+ * Return: a poitner to the new node or NULL if it failed
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *current_node;
+	listint_t *newnode_end;
+	listint_t *firstnode;
 
-	if (head == NULL || new_node == NULL)
+	newnode_end = malloc(sizeof(listint_t));
+	if (newnode_end)
 	{
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if ((*head) == NULL)
-	{
-		(*head) = new_node;
-	}
-	else
-	{
-		current_node = (*head);
-		while (current_node->next != NULL)
+		/* the new node equal to NULL */
+		/* so I know is the new end of the linked list */
+		newnode_end->next = NULL;
+		/* the lenght is n anyway */
+		newnode_end->n = n;
+		if (!*head)
 		{
-			current_node = current_node->next;
+			*head = newnode_end;
+			return (newnode_end);
 		}
-		current_node->next = new_node;
+		/* use the aux variable first node */
+		/* to go through the linked list */
+		firstnode = *head;
+		while (firstnode->next)
+			firstnode = firstnode->next;
+		firstnode->next = newnode_end;
+		return (newnode_end);
 	}
-	return (new_node);
+	/* freed memory if it faile and return NULL */
+	free(newnode_end);
+	return (NULL);
 }
